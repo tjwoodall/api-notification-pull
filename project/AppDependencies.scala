3deb6ebe00dd4627800a8f7f96fccde8515c6c9b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-05-16 13:46:48 +0100

    DCWL-806 Initial upgrade of Scala and Java - code compiles and tests pass

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,10 @@ object AppDependencies {
 
   private val testScope = "test,component"
 
-  val customsApiCommon      = "uk.gov.hmrc"                                %% "customs-api-common"  % "1.57.0"     withSources()
+  val customsApiCommon      = "uk.gov.hmrc"                                %% "customs-api-common"  % "1.59.0"     withSources()
   val scalaTestPlusPlay     = "org.scalatestplus.play"                     %% "scalatestplus-play"  % "5.1.0"    % testScope
   val wireMock              = "com.github.tomakehurst"                     %  "wiremock-standalone" % "2.27.2"   % testScope
   val flexmark              = "com.vladsch.flexmark"                       %  "flexmark-all"        % "0.35.10"  % testScope
   val mockito               = "org.scalatestplus"                          %% "mockito-3-4"         % "3.2.10.0" % testScope
-  val customsApiCommonTests = "uk.gov.hmrc"                                %% "customs-api-common"  % "1.57.0"   % testScope classifier "tests"
-  val silencerPlugin        = compilerPlugin("com.github.ghik" %  "silencer-plugin"     % "1.7.11"     cross CrossVersion.full)
-  val silencerLib           = "com.github.ghik"                            %  "silencer-lib"        % "1.7.11"   % Provided cross CrossVersion.full
+  val customsApiCommonTests = "uk.gov.hmrc"                                %% "customs-api-common"  % "1.59.0"   % testScope classifier "tests"
 }
